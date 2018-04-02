09f26c1b72a9aeefccd1213e73e1861c886ada88
Masahito Okubo 'Mozy <mozy.okubo@gmail.com>
2018-04-03 00:37:37 +0900

    add: app controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,24 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
 end
+
+def new
+  @post = Post.new
+end
+
+def create
+    @post = Post.new(post_params)
+    @post.save
+    redirect_to @post
+end
+
+def show
+  @post = Post.find(params[:id])
+  @post.save!
+end
+
+private
+
+def post_params
+   params.require(:post).permit(:content)
+end
